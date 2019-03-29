4a6b254f7b3e5466020f8768393397bf0680e95c
Reuben Staley <lighthousemaniac@gmail.com>
2019-03-29 17:07:16 -0600

    Two vysyrytâ

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from utils import get_yaml_contents
 bot = commands.Bot(command_prefix='st!')
 
 @bot.command()
-async def emojify(ctx, s):
+async def emojify(ctx, s: str):
     e = ":exclamation:"
     q = ":question:"
     n = ":interrobang:"
     ri = ":regional_indicator_{}:"
     
     result = []
-    for i in s:
+    for i in s.lower():
         last = None;
         try: last = result[-1]
         except IndexError: pass
@@ -49,10 +49,43 @@ async def emojify(ctx, s):
         else: result.append(ri.format(i))
     await ctx.send(" ".join(result))
 
+@bot.command()
+async def flt(ctx, s: str):
+    swaps = ["bd", "cg", "ef", "hn", "it", "jl", "mw", "oq", "px", "uv"]
+    message = ""
+    
+    for char in s:
+        for pair in swaps:
+            i = None
+            try: i = ~-pair.index(char.lower())
+            except ValueError: continue
+
+            message = message + (pair[i] if char == char.lower() else pair[i].upper())
+            break
+        else: message = message + char
+    await ctx.send(message)
+
+@bot.command()
+async def nkt(ctx, dist: int, s: str):
+    keyboard = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
+    message = ""
+
+    for char in s:
+        for row in keyboard:
+            i = None
+            try: i = row.index(char.lower()) + dist
+            except ValueError: continue
+
+            i = i % len(row)
+            message = message + (row[i] if char == char.lower() else row[i].upper())
+            break
+        else: message = message + char
+    await ctx.send(message)
+
 bot.remove_command("help")
 
 @bot.command()
-async def help(ctx, topic=""):
+async def help(ctx, topic: str = ""):
     if topic == "":
         await ctx.send("List of help topics:\n\n{}".format(
             "\n".join(listdir("help"))
@@ -71,7 +104,7 @@ async def help(ctx, topic=""):
         try:
             message = message + "__**Examples:**__\n\n"
             for ex in f["examples"]:
-                message = message + "input: `st!{} {}`\n\noutput: {}".format(
+                message = message + "**input:** `st!{} {}`\n**output:** {}".format(
                     topic, ex["in"], ex["out"]
                 )
         except KeyError: pass
